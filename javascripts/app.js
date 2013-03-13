a089a64ebbdb57c979abbc26514d3c17c5230650
Adriaan Bouman <adriaan.bouman@gmail.com>
2013-03-13 20:42:41 +0100

    isolated on directive and watch instead of attribute $observe

--- javascripts/app.js
+++ javascripts/app.js
@@ -27,7 +27,9 @@ myApp.directive('jsonTree', ['$compile', function ($compile) {
     var directiveDefinitionObject = {
         replace: false,
         restrict: 'A',
-        scope: true,
+        scope: {
+            jsonData: '@jsonTree'
+        },
         compile: function (element, attrs) {
             return {
                 pre: function (scope, element, attrs) {
@@ -72,6 +74,24 @@ myApp.directive('jsonTree', ['$compile', function ($compile) {
                     scope.build = function (json) {
                         if (json) return traverse(angular.fromJson(json));
                     };
+
+                    scope.$watch('jsonData', function (newVal, oldVal) {
+                        if (newVal != oldVal) {
+                            var treeMarkup = scope.build(JSON.parse(newVal));
+                            treeMarkup = angular.element(treeMarkup).prepend('<li><a href="#" ng-click="expandAll($event)">(expand all)</a></li>');
+
+                            element.html("").append($compile(treeMarkup)(scope).addClass('json-tree'));
+                        }
+                    });
+
+                    /*scope.$observe('jsonData', function(jsonStr) {
+                        if (jsonStr) {
+                            scope.jsonData = JSON.parse(scope.jsonData);
+                            var markup = scope.build(scope.jsonData);
+                            markup = angular.element(markup).prepend('<li><a href="#" ng-click="expandAll($event)">(expand all)</a></li>');
+                            element.html("").append($compile(markup)(scope).addClass('json-tree'));
+                        }
+                    });*/
                 },
                 post: function (scope, element, attrs) {
                     scope.showChilds = function ($event) {
@@ -83,19 +103,6 @@ myApp.directive('jsonTree', ['$compile', function ($compile) {
                         angular.element(document.getElementsByClassName('parent')).toggleClass('open');
                         $event.preventDefault();
                     };
-
-                    attrs.$observe('jsonTree', function(jsonStr) {
-                        if (jsonStr) {
-                            scope.jsonObj = JSON.parse(jsonStr);
-
-                            var markup = scope.build(scope.jsonObj);
-
-                            markup = angular.element(markup).prepend('<li><a href="#" ng-click="expandAll($event)">(expand all)</a></li>');
-
-                            var tree = $compile(markup)(scope);
-                            element.html("").append(tree.addClass('json-tree'));
-                        }
-                    });
                 }
             };
         }
